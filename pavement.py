307a07b6e5176d9dec457371d21302a39d14ca03
Eugene Jen <ejen@Eugenes-MacBook-Pro.local>
2017-10-05 15:22:39 -0400

    still too over engineered.

---
+++ pavement.py
@@ -0,0 +1,35 @@
+from paver.easy import *
+__path__ = path(__file__).abspath().dirname()
+
+
+@task
+def pep8():
+    pep8_options = ''
+    dirs = ['hr_problem', 'tests']
+    for dir in dirs:
+       sh('pep8 %s %s' % (pep8_options, dir), cwd=__path__)
+    
+
+@task 
+def pylint():
+    pylint_options = ''
+    modules = ['hr_problem']
+    for module in modules:
+      sh('pylint %s %s' % (pylint_options, module), cwd=__path__)
+    files = ['tests']
+    for file in files:
+      sh('find %s -iname "*.py" | xargs pylint %s' % (file, pylint_options), cwd=__path__) 
+
+
+@task
+def nosetests():
+    nose_options = ''
+    sh('nosetests %s' % nose_options, cwd=__path__.parent)
+
+
+@task
+@needs('nosetests')
+@needs('pylint')
+@needs('pep8')
+def build():
+    pass
